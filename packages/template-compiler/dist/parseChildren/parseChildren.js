3e008d9d6ceaad84f51a05b406ae26e51fb5be9b
Amresh Kumar <kamresh485@gmail.com>
2024-09-07 22:21:53 +0530

    added parsing for children in JSX

--- packages/template-compiler/dist/parseChildren/parseChildren.js
+++ packages/template-compiler/dist/parseChildren/parseChildren.js
@@ -26,18 +26,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = parseChildren;
 const t = __importStar(require("@babel/types"));
 function parseChildren(path) {
-    const children = path.node.children.map((child) => {
+    const children = [];
+    for (let child of path.node.children) {
+        let prop = undefined;
         if (t.isJSXText(child)) {
-            // Handle text nodes
-            return t.objectExpression([
-                t.objectProperty(t.identifier('type'), t.stringLiteral('text_node')),
-                t.objectProperty(t.identifier('children'), t.arrowFunctionExpression([], t.templateLiteral([t.templateElement({ raw: child.value.trim() })], [])))
-            ]);
+            if (child.value.trim()) {
+                prop = t.objectExpression([
+                    t.objectProperty(t.identifier('type'), t.stringLiteral('text_node')),
+                    t.objectProperty(t.identifier('children'), t.stringLiteral(child.value.trim()))
+                ]);
+            }
         }
         else if (t.isJSXElement(child)) {
-            // Recursively process child JSX elements
-            return t.callExpression(t.identifier('processJSXElement'), [child]);
+            prop = child;
         }
-    });
+        if (prop) {
+            children.push(prop);
+        }
+    }
     return children;
 }
--- packages/template-compiler/src/parseChildren/parseChildren.ts
+++ packages/template-compiler/src/parseChildren/parseChildren.ts
@@ -2,26 +2,26 @@ import { NodePath } from "@babel/core";
 import * as t from '@babel/types';
 
 export default function parseChildren(path: NodePath<t.JSXElement>){
-    const children = path.node.children.map((child) => {
-        if (t.isJSXText(child)) {
-          // Handle text nodes
-          return t.objectExpression([
+    const children:(t.JSXElement | t.ObjectExpression | undefined)[] =  [];
+    for(let child of path.node.children){
+      let prop:t.JSXElement | t.ObjectExpression | undefined = undefined;
+      if (t.isJSXText(child)) {
+        if(child.value.trim()){
+          prop = t.objectExpression([
             t.objectProperty(t.identifier('type'), t.stringLiteral('text_node')),
             t.objectProperty(
               t.identifier('children'),
-              t.arrowFunctionExpression(
-                [],
-                t.templateLiteral(
-                  [t.templateElement({ raw: child.value.trim() })],
-                  []
-                )
-              )
+              t.stringLiteral(child.value.trim())
             )
-          ]);
-        } else if (t.isJSXElement(child)) {
-          // Recursively process child JSX elements
-          return t.callExpression(t.identifier('processJSXElement'), [child]);
+          ]); 
         }
-      });
+      } 
+      else if (t.isJSXElement(child)) {
+        prop = child;
+      }
+      if(prop){
+        children.push(prop);
+      }
+    }
     return children;
 }
